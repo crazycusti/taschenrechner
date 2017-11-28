6296b4452f9205cc946d5f9cb9857eaae74034af
Wynton <crazycusti@googlemail.com>
2017-11-28 20:05:59 +0100

    added first number

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,33 @@ use std::io;
 
 fn main() {
 
-let mut z1:i32 = 5;
-let mut z2:i32 = 6;
-let mut z3:i32 = 7;
+println!("rTaschenrechner 0.10\n");
 
+// init variables
+let mut z1 = String::new();
+
+
+//debug
+println!("z1: {}", z1);
+// println!("z2: {}", z2);
+// println!("z3: {}", z3);
+
+// type in first number
+println!("Gebe die erste Zahl ein!\n");
+
+io::stdin()
+    .read_line(&mut z1)
+    .expect("Konnte Zahl nicht erkennen.");
+
+let y1: i32 = z1
+    .trim()
+    .parse()
+    .expect("Konnte Zahl nicht parsen.");
+
+//debug
+println!("z1: {}", z1);
+// println!("z2: {}", z2);
+// println!("z3: {}", z3);
+
+println!("Danke!");
 }
