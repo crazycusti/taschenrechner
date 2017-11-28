2e144218538f4ed33f46371bb1e7909de479f824
Wynton <crazycusti@googlemail.com>
2017-11-28 21:50:54 +0100

    new.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::ops::{Add, Div, Mul, Sub};
+
 
 fn main() {
 
@@ -31,6 +33,22 @@ let y1: i32 = z1
 // println!("z2: {}", z2);
 // println!("y1: {}", y1);
 
+// type in operand
+
+println!("Gebe den Operanden ein. Erlaubte Zeichen: '+' '-' '*' '/'");
+
+let z3 = String::new();
+io::stdin()
+    .read_line(&mut z3)
+    .expect("Konnte Operanden nicht erkennen.");
+
+//let y3 = z3
+  //  .trim()
+  // .parse()
+  // .expect("Operand konnte nicht geparst werden.");
+
+
+
 // type in second numer
 println!("\nGebe die zweite Zahl ein!");
 
@@ -45,9 +63,17 @@ let y2: i32 = z2
 
 // calculate this shit
 
-let x1: i32 = y1 + y2;
+let x2 = match z3 {
+    "+" => Add::<i32>::add,
+    "-" => Sub::<i32>::sub,
+    "*" => Mul::<i32>::mul,
+    "/" => Div::<i32>::div,
+    _ => panic!("Operand konnt nicht verarbeitet werden."),
+};
 
-println!("\nIhr Ergebnis ist {}", x1);
+let x1 = x2(y1, y2);
+
+// println!("\nIhr Ergebnis ist {}", x1);
 
 // debug, hide it when you release it
 // println!("z1: {}", z1);
@@ -55,6 +81,8 @@ println!("\nIhr Ergebnis ist {}", x1);
 // println!("z2: {}", z2);
 // println!("y2: {}", y2);
 // println!("x1: {}", x1);
+println!("z3: {}", z3);
+//println!("y3: {}", y3);
 
 
 
